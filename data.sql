6b5283181732a0c4a6a6059ad39355bb7187cdef
MUNEEB UL REHMAN <bhatmuneeb.mb@gmail.com>
2022-04-25 17:29:14 +0530

    Added initial data of pokemons

--- data.sql
+++ data.sql
@@ -1,5 +1,9 @@
 /* Populate database with sample data. */
 
-INSERT INTO animals (name) VALUES ('Luna');
-INSERT INTO animals (name) VALUES ('Daisy');
-INSERT INTO animals (name) VALUES ('Charlie');
+insert into animals(id,name_,date_of_birth,escape_attempts,neutered,weight_kg) values (1, 'Agumon', '03-02-2020', 0, TRUE, 10.23);
+
+insert into animals(id,name_,date_of_birth,escape_attempts,neutered,weight_kg) values (2, 'Gabumon', '15-11-2018', 2, TRUE, 8);
+
+insert into animals(id,name_,date_of_birth,escape_attempts,neutered,weight_kg) values (3, 'Pikachu', '07-01-2021', 1, false, 15.04);
+
+insert into animals(id,name_,date_of_birth,escape_attempts,neutered,weight_kg) values (4, 'Devimon', '12-05-2017', 5, true, 11);
