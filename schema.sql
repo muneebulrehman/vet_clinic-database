6615df56fcefb7fb1f83dbc1a89298680ba53067
MUNEEB UL REHMAN <bhatmuneeb.mb@gmail.com>
2022-04-26 18:03:34 +0530

    Updated readme

--- schema.sql
+++ schema.sql
@@ -1,7 +1,7 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE table animals(
-    id INT PRIMARY KEY NOT NULL,
+    id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
     name_ varchar(255) NOT NULL,
     date_of_birth DATE NOT null,
     escape_attempts INT NOT null,
