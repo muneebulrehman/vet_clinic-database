2a3d99f0c3cc48c793a03fbe5d9228e942af495b
MUNEEB UL REHMAN <bhatmuneeb.mb@gmail.com>
2022-04-26 17:14:05 +0530

    Made some changes to data and ran queries to fix it

--- data.sql
+++ data.sql
@@ -8,13 +8,13 @@ insert into animals(id,name_,date_of_birth,escape_attempts,neutered,weight_kg) v
 
 insert into animals(id,name_,date_of_birth,escape_attempts,neutered,weight_kg) values (4, 'Devimon', '12-05-2017', 5, true, 11);
 
-insert into animals(name_,date_of_birth,escape_attempts,neutered,weight_kg) values ('Charmander', '28-02-2020', 0, FALSE, 11);
+insert into animals(name_,date_of_birth,escape_attempts,neutered,weight_kg) values ('Charmander', '28-02-2020', 0, FALSE, -11);
 
-insert into animals(name_,date_of_birth,escape_attempts,neutered,weight_kg) values ('Plantmon', '15-11-2021', 2, TRUE, 5.7);
+insert into animals(name_,date_of_birth,escape_attempts,neutered,weight_kg) values ('Plantmon', '15-11-2021', 2, TRUE, -5.7);
 
-insert into animals(name_,date_of_birth,escape_attempts,neutered,weight_kg) values ('Squirtle', '02-04-1993', 3, FALSE, 12.13);
+insert into animals(name_,date_of_birth,escape_attempts,neutered,weight_kg) values ('Squirtle', '02-04-1993', 3, FALSE, -12.13);
 
-insert into animals(name_,date_of_birth,escape_attempts,neutered,weight_kg) values ('Angemon', '12-06-2005', 1, TRUE, 45);
+insert into animals(name_,date_of_birth,escape_attempts,neutered,weight_kg) values ('Angemon', '12-06-2005', 1, TRUE, -45);
 
 insert into animals(name_,date_of_birth,escape_attempts,neutered,weight_kg) values ('Boarmon', '07-07-2005', 7, TRUE, 20.4);
 
@@ -41,3 +41,30 @@ UPDATE animals SET species = 'pokemon' WHERE species IS NULL;
 COMMIT TRANSACTION;
 
 select * from animals
+
+BEGIN;
+
+DELETE from animals;
+
+select * from animals;
+
+ROLLBACK;
+
+select * from animals;
+
+BEGIN;
+
+DELETE from animals where date_of_birth > '01-01-2022'
+
+SAVEPOINT after_deleting_animals_with_bod_greaterthan_01_01_2022;
+
+UPDATE animals set weight_kg = weight_kg * -1;
+
+ROLLBACK TO after_deleting_animals_with_bod_greaterthan_01_01_2022;
+
+UPDATE animals set weight_kg = weight_kg * -1 where weight_kg < 1;
+
+COMMIT;
+
+select * from animals;
+
