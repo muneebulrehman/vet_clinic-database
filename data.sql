f876e2360e210aa4ab680cda7cd2491365a3add0
MUNEEB UL REHMAN <bhatmuneeb.mb@gmail.com>
2022-04-26 16:31:59 +0530

    Added more data to animals table

--- data.sql
+++ data.sql
@@ -7,3 +7,17 @@ insert into animals(id,name_,date_of_birth,escape_attempts,neutered,weight_kg) v
 insert into animals(id,name_,date_of_birth,escape_attempts,neutered,weight_kg) values (3, 'Pikachu', '07-01-2021', 1, false, 15.04);
 
 insert into animals(id,name_,date_of_birth,escape_attempts,neutered,weight_kg) values (4, 'Devimon', '12-05-2017', 5, true, 11);
+
+insert into animals(name_,date_of_birth,escape_attempts,neutered,weight_kg) values ('Charmander', '28-02-2020', 0, FALSE, 11);
+
+insert into animals(name_,date_of_birth,escape_attempts,neutered,weight_kg) values ('Plantmon', '15-11-2021', 2, TRUE, 5.7);
+
+insert into animals(name_,date_of_birth,escape_attempts,neutered,weight_kg) values ('Squirtle', '02-04-1993', 3, FALSE, 12.13);
+
+insert into animals(name_,date_of_birth,escape_attempts,neutered,weight_kg) values ('Angemon', '12-06-2005', 1, TRUE, 45);
+
+insert into animals(name_,date_of_birth,escape_attempts,neutered,weight_kg) values ('Boarmon', '07-07-2005', 7, TRUE, 20.4);
+
+insert into animals(name_,date_of_birth,escape_attempts,neutered,weight_kg) values ('Blossom', '13-10-1998', 3, TRUE, 17);
+
+insert into animals(name_,date_of_birth,escape_attempts,neutered,weight_kg) values ('Ditto', '14-05-2022', 4, TRUE, 22);
