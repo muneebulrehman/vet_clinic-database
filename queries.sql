a3cd4cd62c4f3b6a0daa8a9e2224e63065da61df
MUNEEB UL REHMAN <bhatmuneeb.mb@gmail.com>
2022-04-28 09:14:21 +0530

    Finished all queries

--- queries.sql
+++ queries.sql
@@ -49,18 +49,21 @@ select * from animals join species on animals.species_id = species.id where spec
 
 -- List all owners and their animals, remember to include those that don't own any animal.
 
+select o.full_name as name, a.name_ as name from owners as o left join animals as a on o.id = a.owners_id;
 
 
 -- How many animals are there per species?
 
-
+select s.name as type, count(a.id ) as total from species as s left join animals as a on s.id = a.species_id group by s.name;
 
 -- List all Digimon owned by Jennifer Orwell.
 
-
+select a.name_ as name , o.full_name as owners_name from animals as a  join owners as o on a.owners_id = o.id where o.full_name = 'Jennifer Orwell';
 
 -- List all animals owned by Dean Winchester that haven't tried to escape.
 
-
+select a.name_ as animal_name, a.escape_attempts as escapes, o.full_name as owner from animals as a join owners as o on a.owners_id = o.id where (o.full_name = 'Dean Winchester' and a.escape_attempts = 0);
 
 -- Who owns the most animals?
+
+select count(a.id) as total_animals, o.full_name as owner from animals as a join owners as o on a.owners_id = o.id group by o.full_name order by total_animals desc limit (1);
